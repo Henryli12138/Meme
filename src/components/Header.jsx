18fe7d90a2327869ee5a52eb91419dfeca992bff
Henry <henryli12138@gmail.com>
2024-02-22 20:11:21 -0700

    Add existing file

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image" src="./images/troll-face.png"/>
+            <h1 className="header--title">Header component</h1>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+        
+    )
+}
